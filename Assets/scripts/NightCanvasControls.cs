470ef4731370e74413d78f3ab9a3e6fcb92a9bf8
bvolsky <53959079+bvolsky@users.noreply.github.com>
2020-04-19 17:39:55 -0400

    Night Controls Canvas prefab

---
+++ Assets/scripts/NightCanvasControls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightCanvasControls : MonoBehaviour
+{
+
+    public GameObject ControlsCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ControlsCanvas.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown("tab"))
+        {
+            ControlsCanvas.SetActive(true);
+
+        }
+        if (Input.GetKeyUp("tab"))
+        {
+            ControlsCanvas.SetActive(false);
+
+        }
+
+
+    }
+}
