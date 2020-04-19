32bc33d034ec0a08114d259fd1c6876e8dbc4a1e
bvolsky <53959079+bvolsky@users.noreply.github.com>
2020-04-18 21:43:49 -0400

    Saturday PM changes
    Revamped UI; highlighting and keyboard input

--- Assets/scripts/DayChoice.cs
+++ Assets/scripts/DayChoice.cs
@@ -12,7 +12,7 @@ public class DayChoice : MonoBehaviour, IPointerClickHandler
     public enum BonusEnum { Health, Sewing, Accelerant, Rest, Melee, Ranged}
     public BonusEnum Bonus;
     public DayScene dayScene;
-    public Vector3 vectorformovingtext;
+   
 
 
     void Start()
@@ -31,83 +31,10 @@ public class DayChoice : MonoBehaviour, IPointerClickHandler
     {
         if ((DayScene.DayTime + ChoiceCost <= DayScene.TimeinaDay) && (dayScene.canvases[5].GetComponentInChildren<SunScript>().Moving == false))
         {
-            bool success = false;
-            string movingstring = "";
-            if (Bonus == BonusEnum.Health)
-            {
-                DayScene.MaxHealth += 10;
-                success = true;
-                movingstring = "+10 Magic Fire Shield";
-                vectorformovingtext = dayScene.canvases[0].transform.position;
-                vectorformovingtext.x = dayScene.canvases[0].transform.position.x+200;
-                vectorformovingtext.y = dayScene.canvases[0].transform.position.y;
-
-            }
-            else if (Bonus == BonusEnum.Sewing)
-            {
-                DayScene.MaxAccelerant += 1;
-                success = true;
-                movingstring = "+1 Heart Slot";
-                vectorformovingtext = dayScene.canvases[1].transform.position;
-                vectorformovingtext.x = dayScene.canvases[1].transform.position.x+200;
-                vectorformovingtext.y = dayScene.canvases[1].transform.position.y;
-            }
-            else if ((Bonus == BonusEnum.Accelerant) && (DayScene.MaxAccelerant > DayScene.AccelerantInHand))
-            {
-                DayScene.AccelerantInHand += 1;
-                success = true;
-                movingstring = "+1 Sacrifical Heart";
-                vectorformovingtext = dayScene.canvases[2].transform.position;
-                vectorformovingtext.x = dayScene.canvases[2].transform.position.x+200;
-                vectorformovingtext.y = dayScene.canvases[2].transform.position.y;
-            }
-            else if (Bonus == BonusEnum.Melee)
-            {
-                DayScene.MeleeWeaponDmg += 3;
-                movingstring = "+3 Melee Damage";
-
-                success = true;
-                vectorformovingtext = dayScene.canvases[7].transform.position;
-                vectorformovingtext.x = dayScene.canvases[7].transform.position.x + 200;
-                vectorformovingtext.y = dayScene.canvases[7].transform.position.y;
-            }
-            else if (Bonus == BonusEnum.Ranged)
-            {
-                DayScene.RangeWeaponDmg += 2;
-                movingstring = "+2 Ranged Damage";
-
-                success = true;
-                vectorformovingtext = dayScene.canvases[8].transform.position;
-                vectorformovingtext.x = dayScene.canvases[8].transform.position.x + 200;
-                vectorformovingtext.y = dayScene.canvases[8].transform.position.y;
+            dayScene.MakeSelection(this.GetComponent<DayChoice>());
 
 
-            }
-            else if (Bonus == BonusEnum.Rest)
-            {
-                success = true;
-                ChoiceCost = (DayScene.TimeinaDay - DayScene.DayTime);
-                movingstring = "+" + ((DayScene.TimeinaDay - DayScene.DayTime) * 10) + "% One Night Speed Bonus";
-                DayScene.SpeedBonus = (DayScene.TimeinaDay - DayScene.DayTime) * 10;
-                vectorformovingtext = dayScene.canvases[4].transform.position;
-                vectorformovingtext.x = dayScene.canvases[4].transform.position.x;
-                vectorformovingtext.y = dayScene.canvases[4].transform.position.y;
-            }
-            //Play animation?
-            if (success)
-            {
-                DayScene.DayTime += ChoiceCost;
-                dayScene.canvases[5].GetComponentInChildren<SunScript>().Move(ChoiceCost);
-                FindObjectOfType<MovingTextScript>().GetComponent<Text>().text = movingstring;
-                FindObjectOfType<MovingTextScript>().Move(vectorformovingtext, 1.0f, new Vector3(dayScene.canvases[3].transform.position.x, dayScene.canvases[3].transform.position.y + 100, dayScene.canvases[3].transform.position.z));
-                
-                dayScene.canvases[4].GetComponentInChildren<Text>().text = "Rest Until Dawn";
-                if (DayScene.DayTime < DayScene.TimeinaDay)
-                {
-                    dayScene.canvases[4].GetComponentInChildren<Text>().text = dayScene.canvases[4].GetComponentInChildren<Text>().text + "\n(Gain Temporary " + ((DayScene.TimeinaDay - DayScene.DayTime) * 10) + "% Speed Bonus)";
-                }
-            }
-            
+               
         }
     }
 
--- Assets/scripts/SunScript.cs
+++ Assets/scripts/SunScript.cs
@@ -27,17 +27,19 @@ public class SunScript : MonoBehaviour
             if (LerpTime >= LagTime)
             {
                 Moving = false;
-                transform.position = EndingPosition;
+                transform.localPosition = EndingPosition;
                 dayScene.canvases[3].GetComponentInChildren<Text>().text = "\tMagic Circle Max Health: " + DayScene.MaxHealth + "\n\tHeart Pouches: " + DayScene.MaxAccelerant +
                     "\n\tSacrificial Hearts: " + DayScene.AccelerantInHand + "\n\tMelee Attack: " + DayScene.MeleeWeaponDmg + "\n\tRanged Attack: " + DayScene.RangeWeaponDmg;
                 if (DayScene.DayTime >= DayScene.TimeinaDay)
                 {
                     dayScene.timeron = true;
+                    dayScene.SelectPossible = false;
+
                 }
             }
             else
             {
-                transform.position = Vector3.Lerp(StartingPosition, EndingPosition, LerpTime / LagTime);
+                transform.localPosition = Vector3.Lerp(StartingPosition, EndingPosition, LerpTime / LagTime);
             }
         }
     }
@@ -45,8 +47,10 @@ public class SunScript : MonoBehaviour
     public void Move(int x)
     {
         LagTime = (x * 0.4f);
-        StartingPosition = this.transform.position;
-        EndingPosition = new Vector3(StartingPosition.x + (100 * x), this.transform.position.y, this.transform.position.z);
+        StartingPosition = this.transform.localPosition;
+        float width = dayScene.canvases[5].GetComponent<RectTransform>().rect.width;
+        float offset = (width / 9) * x;
+        EndingPosition = new Vector3(StartingPosition.x + offset, this.transform.localPosition.y, this.transform.localPosition.z);
         Moving = true;
         LerpTime = 0.0f;
     }
